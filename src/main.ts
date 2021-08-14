0a6713c9dca4337b5035df540b9041b93016c088
SoupMctavish <75652026+BadilloSanchezJose12@users.noreply.github.com>
2021-08-13 19:23:06 -0500

    4k

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   //await app.listen(3000);
-  app.enableCors({
-    origin: '*',
-    methods: 'GET, PUT, POST, DELETE',
-    allowedHeaders: 'Content-Type, Authorization',
-});
+  
   await app.listen(process.env.PORT || 8080);
 }
 bootstrap();
